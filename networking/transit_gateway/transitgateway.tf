f87f131b1dcdf1d65fa3785b5eda704994c02f43
Gaurav Raje <gaurav.raje07@gmail.com>
2020-10-16 23:40:38 -0400

    Attach production

--- networking/transit_gateway/transitgateway.tf
+++ networking/transit_gateway/transitgateway.tf
@@ -1,3 +1,8 @@
 resource "aws_ec2_transit_gateway" "example" {
   description = "example"
 }
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
+  transit_gateway_id = aws_ec2_transit_gateway.example.id
+  vpc_id             = aws_vpc.production.id
+}
