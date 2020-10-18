4c1c1e4c310d7ce251112502735f16755d593e4b
chimbs86 <gaurav.raje07@gmail.com>
2020-10-18 02:25:37 -0400

    remove nlb

--- networking/privatelink/production.tf
+++ networking/privatelink/production.tf
@@ -27,15 +27,15 @@ resource "aws_vpc_peering_connection" "foo" {
   }
 }
 
-resource "aws_lb" "load_balancer" {
-  name = "test-network-lb"
-  #can also be obtained from the variable nlb_config
-  load_balancer_type = "network"
-  internal = true
-  subnet_mapping {
-    subnet_id = aws_subnet.production_customer.id
-  }
-  tags = {
-    Environment = "prod"
-  }
-}
+//resource "aws_lb" "load_balancer" {
+//  name = "test-network-lb"
+//  #can also be obtained from the variable nlb_config
+//  load_balancer_type = "network"
+//  internal = true
+//  subnet_mapping {
+//    subnet_id = aws_subnet.production_customer.id
+//  }
+//  tags = {
+//    Environment = "prod"
+//  }
+//}
